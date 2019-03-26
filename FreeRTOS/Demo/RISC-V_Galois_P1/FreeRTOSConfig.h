56913b8b5ec5d4cded5cf224cbdb405eebbf183e
Max Waugaman <mwaugaman@galois.com>
2019-03-26 15:00:06 -0700

    Updated default frequency to 50MHz

--- FreeRTOS/Demo/RISC-V_Galois_P1/FreeRTOSConfig.h
+++ FreeRTOS/Demo/RISC-V_Galois_P1/FreeRTOSConfig.h
@@ -87,7 +87,7 @@
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				1
-#define configCPU_CLOCK_HZ				( ( uint32_t ) ( 83000000 ) )
+#define configCPU_CLOCK_HZ				( ( uint32_t ) ( 50000000 ) )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
 #define configMINIMAL_STACK_SIZE		( ( uint32_t ) 512 ) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
